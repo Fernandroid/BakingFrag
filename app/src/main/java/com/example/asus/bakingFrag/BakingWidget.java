5e7e5e4f8c63d2418159689c907166a3610b8581
Fernandroid <gfernag@gmail.com>
2018-06-19 19:36:36 +0200

    add no data available text in widget

--- app/src/main/java/com/example/asus/bakingFrag/BakingWidget.java
+++ app/src/main/java/com/example/asus/bakingFrag/BakingWidget.java
@@ -6,7 +6,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import com.google.gson.Gson;
@@ -23,15 +25,28 @@ public class BakingWidget extends AppWidgetProvider {
         Timber.i("update widget called");
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
-        String recipeName=getRecipeName(context);
-        views.setTextViewText(R.id.recipe_name_widget,recipeName);
-        // Set the service to act as adapter of ListView
-        Intent sendService=new Intent(context,BakingWidgetService.class);
-        views.setRemoteAdapter(R.id.appwidget_listview,sendService);
-        // Set the MainActivity intent to launch when clicked
-        Intent appIntent = new Intent(context, MainActivity.class);
-        PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setPendingIntentTemplate(R.id.appwidget_listview, appPendingIntent);
+        // get the last displayed recipe by user in RecipeDetail
+        String recipeName= getRecipeName(context);
+        if(recipeName!=null){
+            // diplay the name of the recipe
+            views.setTextViewText(R.id.recipe_name_widget,recipeName);
+            // Set the service to act as adapter of ListView
+            Intent sendService=new Intent(context,BakingWidgetService.class);
+            views.setRemoteAdapter(R.id.appwidget_listview,sendService);
+            // Set the RecipeDeail intent to launch when clicked
+            Intent appIntent = new Intent(context, RecipeDetail.class);
+            PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.appwidget_listview, appPendingIntent);
+        } else {
+           views.setTextViewText(R.id.no_data_widget,context.getString(R.string.no_data_widget_key));
+           //Set visibility to see the text no data and invisibility to hide recycler
+             views.setViewVisibility(R.id.no_data_widget, View.VISIBLE);
+             views.setViewVisibility(R.id.appwidget_listview,View.INVISIBLE);
+             // Intent to open MainActivity to look recipe list
+            Intent openMainActivity = new Intent(context, MainActivity.class);
+            PendingIntent openPendingIntent = PendingIntent.getActivity(context, 0, openMainActivity, 0);
+            views.setOnClickPendingIntent(R.id.no_data_widget,openPendingIntent);
+        }
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -44,10 +59,10 @@ public class BakingWidget extends AppWidgetProvider {
         }
     }
 
+    // Get the recipe that user has displayed in RecipeDetail acitivity
     public static String getRecipeName(Context context) {
         Gson gson = new Gson();
         SharedPreferences sharedPref =context.getSharedPreferences(context.getString(R.string.preferece_file_key), context.MODE_PRIVATE);
-        String stringRecipe = sharedPref.getString(context.getString(R.string.file_json_key), null);
         return sharedPref.getString(context.getString(R.string.recipe_name_key), null);
     }
     @Override
